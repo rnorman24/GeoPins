843ddcbc84ce9b6f4a26dd3e5d036a58929d20d7
unknown <nray24@gmail.com>
2019-05-23 15:08:38 -0600

    refactor type defs

--- typeDefs.js
+++ typeDefs.js
@@ -8,23 +8,23 @@ module.exports = gql`
     picture: String
   }
 
-type Pin {
-  _id: ID
-  createdAt: String
-  title: String
-  content: String
-  image: String
-  latitude: Float
-  longitude: Float
-  author: User
-  comments: [Comment]
-}
+  type Pin {
+    _id: ID
+    createdAt: String
+    title: String
+    content: String
+    image: String
+    latitude: Float
+    longitude: Float
+    author: User
+    comments: [Comment]
+  }
 
-type Comment {
-  text: String
-  createdAt: String
-  author: User
-}
+  type Comment {
+    text: String
+    createdAt: String
+    author: User
+  }
 
   type Query {
     me: User
