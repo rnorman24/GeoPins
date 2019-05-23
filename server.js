a70a1c025986fb6eb4d52692dde6f23c7db91ee7
unknown <nray24@gmail.com>
2019-05-23 14:57:26 -0600

    refactor connected db

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const { ApolloServer } = require("apollo-server");
 
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
-  .then(() => console.log('DB connected!'))
-  .catch(err => console.error(err))
+  .then(() => console.log("DB connected!"))
+  .catch(err => console.error(err));
 
 const server = new ApolloServer({
   typeDefs,
@@ -16,5 +16,5 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ url }) => {
-  console.log(`Server listening on ${url}`)
+  console.log(`Server listening on ${url}`);
 });
