0f1926dbeda446abcf9a14db4b86d42afbb9dbbc
unknown <nray24@gmail.com>
2019-05-24 10:40:17 -0600

    add oauth env

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -5,13 +5,15 @@ import { withStyles } from "@material-ui/core/styles";
 
 const Login = ({ classes }) => {
   const onSuccess = googleUser => {
-    console.log({ googleUser });
+    const idToken = googleUser.getAuthResponse().id_token
+    console.log({ idToken })
   };
 
   return (
     <GoogleLogin
       clientId="211278434930-vunn4kbu6h2bgjgbcm9fkk7agrddc0nd.apps.googleusercontent.com"
       onSuccess={onSuccess}
+      isSignedIn={true}
     />
   );
 };
