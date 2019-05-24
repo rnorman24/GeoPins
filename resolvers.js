fddc91c3710ea306cbbb25a3ed564ce54dff3501
unknown <nray24@gmail.com>
2019-05-24 11:44:14 -0600

    refactor authentication

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server')
+const { AuthenticationError } = require("apollo-server");
 
 const user = {
   _id: "1",
@@ -9,13 +9,13 @@ const user = {
 
 const authenticated = next => (root, args, ctx, info) => {
   if (!ctx.currentUser) {
-    throw new AuthenticationError('You must be logged in')
+    throw new AuthenticationError("You must be logged in");
   }
-  return next()
-}
+  return next(root, args, ctx, info);
+};
 
 module.exports = {
   Query: {
-    me: authenticated(() => ctx.currentUser)
+    me: authenticated((root, args, ctx) => ctx.currentUser)
   }
 };
