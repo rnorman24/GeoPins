73c85d95d54a7799a215113d879097f8a23d0cde
unknown <nray24@gmail.com>
2019-05-24 11:32:57 -0600

    add authentication

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require('apollo-server')
+
 const user = {
   _id: "1",
   name: "Reed",
@@ -5,8 +7,15 @@ const user = {
   picture: "https://cloudinary.com/asdf"
 };
 
+const authenticated = next => (root, args, ctx, info) => {
+  if (!ctx.currentUser) {
+    throw new AuthenticationError('You must be logged in')
+  }
+  return next()
+}
+
 module.exports = {
   Query: {
-    me: () => user
+    me: authenticated(() => ctx.currentUser)
   }
 };
