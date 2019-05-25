8bb9cfd830ce3a3f46ed1c9db067f4f89f6e34aa
unknown <nray24@gmail.com>
2019-05-25 09:19:41 -0600

    refactor server.js

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 const { findOrCreateUser } = require("./controllers/userController");
 
-require("dotenv").config();
-
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("DB connected!"))
