aa11aecd03a00f8b16c5544eef886df03b4d9554
unknown <nray24@gmail.com>
2019-05-29 15:10:34 -0600

    refactor map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
@@ -9,21 +9,20 @@ const viewport = {
   latitude: 37.7577,
   longitude: -122.4376,
   zoom: 13
-}
+};
 
 const Map = ({ classes }) => {
   return (
-  <div className={classes.root}>
-    <ReactMapGL
-      width='100vw'
-      height='calc(100vh - 64px)'
-      mapStyle='mapbox://styles/mapbox/streets-v9'
-      mapboxApiAccessToken='pk.eyJ1IjoibnJheTI0IiwiYSI6ImNqdzEzNnJzdzAydTQ0YW81dG1mM2l4bWQifQ.yVp11VFdmvMLZM82IJm5JQ'
-      {...viewport}
-    >
-
-    </ReactMapGL>
-  </div>);
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoibnJheTI0IiwiYSI6ImNqdzEzNnJzdzAydTQ0YW81dG1mM2l4bWQifQ.yVp11VFdmvMLZM82IJm5JQ"
+        {...viewport}
+      />
+    </div>
+  );
 };
 
 const styles = {
--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 import Header from "../components/Header";
-import Map from '../components/Map'
+import Map from "../components/Map";
 import withRoot from "../withRoot";
 
 const App = () => {
   return (
-  <>
-  <Header />
-  <Map />
-  </>
-)
+    <>
+      <Header />
+      <Map />
+    </>
+  );
 };
 
 export default withRoot(App);
