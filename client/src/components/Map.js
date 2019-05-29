21bd5bbf5c9c292e175cebd14197571c4a031564
unknown <nray24@gmail.com>
2019-05-29 15:49:32 -0600

    add pin icon

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import ReactMapGL, { NavigationControl } from "react-map-gl";
+import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+
+import PinIcon from './PinIcon'
 
 const INITIAL_VIEWPORT = {
   latitude: 37.7577,
@@ -44,6 +46,18 @@ const Map = ({ classes }) => {
             onViewportChange={newViewport => setViewport(newViewport)}
           />
         </div>
+
+        {/* Pin for User's Current Position */}
+        {userPosition && (
+          <Marker
+            latitude={userPosition.latitude}
+            longitude={userPosition.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color='red' />
+          </Marker>
+        )}
       </ReactMapGL>
     </div>
   );
