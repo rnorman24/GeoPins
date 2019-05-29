c5f8a8e678ab85ae7f95dce8a056e3cc46c3c61e
unknown <nray24@gmail.com>
2019-05-29 15:15:39 -0600

    add viewport scroll

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
-const viewport = {
+const INITIAL_VIEWPORT = {
   latitude: 37.7577,
   longitude: -122.4376,
   zoom: 13
 };
 
 const Map = ({ classes }) => {
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT)
+
   return (
     <div className={classes.root}>
       <ReactMapGL
@@ -19,6 +21,7 @@ const Map = ({ classes }) => {
         height="calc(100vh - 64px)"
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxApiAccessToken="pk.eyJ1IjoibnJheTI0IiwiYSI6ImNqdzEzNnJzdzAydTQ0YW81dG1mM2l4bWQifQ.yVp11VFdmvMLZM82IJm5JQ"
+        onViewportChange={newViewport => setViewport(newViewport)}
         {...viewport}
       />
     </div>
