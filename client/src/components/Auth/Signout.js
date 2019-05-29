b29a5ef9ac1a41ff04dcfbd6f5d500759c30d6f6
unknown <nray24@gmail.com>
2019-05-29 14:44:39 -0600

    refactor signout button

--- client/src/components/Auth/Signout.js
+++ client/src/components/Auth/Signout.js
@@ -1,35 +1,32 @@
 import React, { useContext } from "react";
-import { GoogleLogout } from 'react-google-login'
+import { GoogleLogout } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import Typography from "@material-ui/core/Typography";
 
-import Context from '../../context'
+import Context from "../../context";
 
 const Signout = ({ classes }) => {
-  const { dispatch } = useContext(Context)
+  const { dispatch } = useContext(Context);
 
   const onSignout = () => {
-    dispatch({ type: 'SIGNOUT_USER' })
-    console.log('Signed out user')
-  }
+    dispatch({ type: "SIGNOUT_USER" });
+    console.log("Signed out user");
+  };
 
   return (
     <GoogleLogout
       onLogoutSuccess={onSignout}
       render={({ onClick }) => (
         <span className={classes.root} onClick={onClick}>
-          <Typography
-            variant='body1'
-            className={classes.buttonText}
-          >
+          <Typography variant="body1" className={classes.buttonText}>
             Signout
           </Typography>
           <ExitToAppIcon className={classes.buttonIcon} />
         </span>
       )}
     />
-  )
+  );
 };
 
 const styles = {
