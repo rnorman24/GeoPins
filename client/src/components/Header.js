f88a4b37f686f1997b3654b8572f5af20c95689d
unknown <nray24@gmail.com>
2019-05-29 14:44:11 -0600

    add signout button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,6 +6,7 @@ import MapIcon from "@material-ui/icons/Map";
 import Typography from "@material-ui/core/Typography";
 
 import Context from "../context";
+import Signout from '../components/Auth/Signout'
 
 const Header = ({ classes }) => {
   const { state } = useContext(Context);
@@ -36,6 +37,7 @@ const Header = ({ classes }) => {
           )}
 
           {/* Signout Button */}
+          <Signout />
         </Toolbar>
       </AppBar>
     </div>
