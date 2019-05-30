f32a9dc6a3a7238602fa2b6019b638a3dc00fe6f
unknown <nray24@gmail.com>
2019-05-30 11:40:04 -0600

    add pin content state

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
@@ -9,6 +9,15 @@ import ClearIcon from "@material-ui/icons/Clear";
 import SaveIcon from "@material-ui/icons/SaveTwoTone";
 
 const CreatePin = ({ classes }) => {
+  const [title, setTitle] = useState('')
+  const [image, setImage] = useState('')
+  const [content, setContent] = useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log({ title, image, content })
+  }
+
   return (
     <form className={classes.form}>
       <Typography
@@ -20,12 +29,15 @@ const CreatePin = ({ classes }) => {
         <LandscapeIcon className={classes.iconLarge} /> Pin Location
       </Typography>
       <div>
-        <TextField name="title" label="Title" placeholder="Insert pin title" />
+        <TextField name="title" label="Title" placeholder="Insert pin title"
+          onChange={e => setTitle(e.target.value)}
+        />
         <input
           accept="image/*"
           id="image"
           type="file"
           className={classes.input}
+          onChange={e => setImage(e.target.files[0])}
         />
         <label htmlFor="image">
           <Button component="span" size="small" className={classes.button}>
@@ -42,6 +54,7 @@ const CreatePin = ({ classes }) => {
           margin="normal"
           fullWidth
           variant="outlined"
+          onChange={e => setContent(e.target.value)}
         />
       </div>
       <div>
@@ -54,6 +67,8 @@ const CreatePin = ({ classes }) => {
           className={classes.button}
           variant="contained"
           color="secondary"
+          disabled={!title.trim() || !content.trim() || !image}
+          onClick={handleSubmit}
         >
           Submit
           <SaveIcon className={classes.rightIcon} />
