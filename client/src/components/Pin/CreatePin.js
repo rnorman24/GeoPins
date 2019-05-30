573fd272e9ff6607b538883c4538e7f4502cc8a1
unknown <nray24@gmail.com>
2019-05-30 12:10:40 -0600

    refactor pin delete

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -8,20 +8,20 @@ import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
 import ClearIcon from "@material-ui/icons/Clear";
 import SaveIcon from "@material-ui/icons/SaveTwoTone";
 
-import Context from '../../context'
+import Context from "../../context";
 
 const CreatePin = ({ classes }) => {
-  const { dispatch } = useContext(Context)
+  const { dispatch } = useContext(Context);
   const [title, setTitle] = useState("");
   const [image, setImage] = useState("");
   const [content, setContent] = useState("");
 
   const handleDeleteDraft = () => {
-    setTitle('')
-    setImage('')
-    setContent('')
-    dispatch({ type: 'DELETE_DRAFT' })
-  }
+    setTitle("");
+    setImage("");
+    setContent("");
+    dispatch({ type: "DELETE_DRAFT" });
+  };
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -76,7 +76,12 @@ const CreatePin = ({ classes }) => {
         />
       </div>
       <div>
-        <Button onClick={handleDeleteDraft} className={classes.button} variant="contained" color="primary">
+        <Button
+          onClick={handleDeleteDraft}
+          className={classes.button}
+          variant="contained"
+          color="primary"
+        >
           <ClearIcon className={classes.leftIcon} />
           Discard
         </Button>
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -33,7 +33,7 @@ export default function reducer(state, { type, payload }) {
       return {
         ...state,
         draft: null
-      }
+      };
     default:
       return state;
   }
