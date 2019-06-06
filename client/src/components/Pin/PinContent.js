4d107ea555cf4516354e90b4c69b602e28322824
unknown <nray24@gmail.com>
2019-06-06 11:07:28 -0600

    add date format

--- client/src/components/Pin/PinContent.js
+++ client/src/components/Pin/PinContent.js
@@ -3,6 +3,7 @@ import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import FaceIcon from "@material-ui/icons/Face";
+import format from 'date-fns/format'
 
 import Context from "../../context";
 
@@ -30,7 +31,7 @@ const PinContent = ({ classes }) => {
         gutterBottom
       >
         <AccessTimeIcon className={classes.icon} />
-        {createdAt}
+        {format(Number(createdAt), 'MMM Do, YYYY')}
       </Typography>
       <Typography variant="subtitle1" gutterBottom>
         {content}
