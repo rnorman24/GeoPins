e5b4c826c9bbf65eca8ffd1d449b2e464dc8ed4d
unknown <nray24@gmail.com>
2019-06-07 12:30:23 -0600

    add comment date to now

--- client/src/components/Comment/Comments.js
+++ client/src/components/Comment/Comments.js
@@ -6,6 +6,7 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
 
 const Comments = ({ comments, classes }) => (
   <List className={classes.root}>
@@ -17,6 +18,7 @@ const Comments = ({ comments, classes }) => (
         <ListItemText
           primary={comment.text}
           secondary={
+            <>
             <Typography
               className={classes.inline}
               component="span"
@@ -24,6 +26,8 @@ const Comments = ({ comments, classes }) => (
             >
               {comment.author.name}
             </Typography>
+             · {distanceInWordsToNow(Number(comment.createdAt))} ago
+            </>
           }
         />
       </ListItem>
