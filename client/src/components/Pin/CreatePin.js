4592a18e66d743925bd917ee5ec020655c24e8c9
unknown <nray24@gmail.com>
2019-06-07 14:48:53 -0600

    refactor live data changes with apollo client

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -47,10 +47,7 @@ const CreatePin = ({ classes }) => {
       const url = await handleImageUpload();
       const { latitude, longitude } = state.draft;
       const variables = { title, image: url, content, latitude, longitude };
-      await client.request(
-        CREATE_PIN_MUTATION,
-        variables
-      );
+      await client.request(CREATE_PIN_MUTATION, variables);
       handleDeleteDraft();
     } catch (err) {
       setSubmitting(false);
