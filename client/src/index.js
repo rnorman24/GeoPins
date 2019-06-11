973e322c6251f03c0beba7cd11cb5f89324d7b55
unknown <nray24@gmail.com>
2019-06-11 11:19:25 -0600

    heroku deployment

--- client/src/index.js
+++ client/src/index.js
@@ -17,7 +17,7 @@ import { WebSocketLink } from "apollo-link-ws";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 const wsLink = new WebSocketLink({
-  uri: "wss://geopinpopup.heroku.com/graphql",
+  uri: "wss://geopinpopup.herokuapp.com/graphql",
   options: {
     reconnect: true
   }
